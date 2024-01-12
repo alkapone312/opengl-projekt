9c432a943249006f11d10b433b50f50914021619
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2024-01-13 00:55:45 +0100

    Add automatic channel color recognition

--- Texture.py
+++ Texture.py
@@ -3,11 +3,12 @@ from OpenGL.GL import *
 from Shader import Shader
 
 class Texture:
-    def __init__(self, texPath, texType, slot, format, pixelType):
+    def __init__(self, texPath, texType, slot):
         self.texType = texType
         textureImg = pg.image.load(texPath)
         textureImg = pg.transform.flip(textureImg, False, True)
         width, height = textureImg.get_size()
+        numberOfColorChannels = textureImg.get_bytesize()
         bytes = textureImg.get_buffer().raw
         self.ID = glGenTextures(1)
         glActiveTexture(GL_TEXTURE0 + slot)
@@ -18,7 +19,17 @@ class Texture:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, pixelType, bytes)
+        if texType == 'normal':
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes)
+        elif numberOfColorChannels == 4:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes)
+        elif numberOfColorChannels == 3:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes)
+        elif numberOfColorChannels == 1:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, bytes)
+        else:
+            print("Cannot determine texture type.")
+            exit()
         glGenerateMipmap(GL_TEXTURE_2D)
         
         glBindTexture(GL_TEXTURE_2D, 0)
